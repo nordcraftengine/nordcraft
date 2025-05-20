fd847b73426eb54064662a277038639751b61a6b
Erik Beuschau <bager.erik@gmail.com>
2025-05-20 10:40:07 +0200

    Report unused eslint-disable directives

--- packages/core/src/formula/formulaTypes.ts
+++ packages/core/src/formula/formulaTypes.ts
@@ -20,13 +20,11 @@ export interface ToddleFormula extends BaseFormula {
 /**
  * The Handler generic is a string server side, but a function client side
  */
-// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export interface CodeFormula<Handler = string | Function> extends BaseFormula {
   version?: 2 | never
   handler: Handler
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export type PluginFormula<Handler = string | Function> =
   | ToddleFormula
   | CodeFormula<Handler>
@@ -35,7 +33,6 @@ export const isToddleFormula = <Handler>(
   formula: PluginFormula<Handler>,
 ): formula is ToddleFormula => Object.hasOwn(formula, 'formula')
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export interface GlobalFormulas<Handler = string | Function> {
   formulas?: Record<string, PluginFormula<Handler>>
   packages?: Partial<
