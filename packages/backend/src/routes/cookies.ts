52f3aa666e5ae3ab4f1cecf5dec646d55ed3ca37
Erik Beuschau <bager.erik@gmail.com>
2025-05-22 13:30:03 +0200

    Add set-cookie endpoint + include ttl as parameter

---
+++ packages/backend/src/routes/cookies.ts
@@ -0,0 +1,101 @@
+import { escapeSearchParameters } from '@nordcraft/ssr/dist/rendering/request'
+import type { Handler } from 'hono'
+import type { HonoEnv } from '../../hono'
+
+export const setCookieHandler: Handler<HonoEnv> = async (ctx) => {
+  const url = new URL(ctx.req.url)
+  const searchParameters = escapeSearchParameters(url.searchParams)
+  const name = searchParameters.get('name')
+  const value = searchParameters.get('value')
+  const sameSite = searchParameters.get('sameSite') ?? 'Lax'
+  const path = searchParameters.get('path') ?? '/'
+  const ttl = searchParameters.get('ttl')
+  const includeSubdomains =
+    searchParameters.get('includeSubdomains') !== 'false' // default to true
+
+  if (typeof name !== 'string' || name === '') {
+    return ctx.json(
+      { error: `Bad request. The name must be of type string.` },
+      {
+        status: 400,
+      },
+    )
+  }
+  if (typeof value !== 'string') {
+    return ctx.json(
+      { error: `Bad request. The value must be of type string.` },
+      {
+        status: 400,
+      },
+    )
+  }
+  if (['lax', 'strict', 'none'].includes(sameSite.toLowerCase()) === false) {
+    return ctx.json(
+      { error: `Bad request. The sameSite must be "Lax", "Strict" or "None".` },
+      {
+        status: 400,
+      },
+    )
+  }
+  if (typeof path !== 'string' || !path.startsWith('/')) {
+    return ctx.json(
+      {
+        error: `Bad request. The path must be of type string and start with /.`,
+      },
+      {
+        status: 400,
+      },
+    )
+  }
+  if (typeof ttl === 'string' && isNaN(Number(ttl))) {
+    return ctx.json(
+      { error: `Bad request. The ttl must be a valid number.` },
+      {
+        status: 400,
+      },
+    )
+  }
+
+  let expirationDate = new Date(
+    // Prefer the ttl from the search parameters, but default to 1 hour
+    Date.now() + (typeof ttl === 'string' ? Number(ttl) : 3600) * 1000,
+  )
+  if (typeof ttl !== 'string') {
+    // If no ttl was provided, in case this is a jwt, we determine the expiry date from the payload
+    const accessTokenPayload = decodeToken(value)
+    if (typeof accessTokenPayload?.exp === 'number') {
+      expirationDate = new Date(accessTokenPayload.exp * 1000)
+    }
+  }
+
+  const headers = new Headers()
+  const expires = expirationDate.toUTCString()
+  // We will always set the cookie to Secure. Anything else can be configured
+  let cookieHeader = `${name}=${value}; Expires=${expires}; Secure; HttpOnly; SameSite=${sameSite}; Path=${path}`
+  if (includeSubdomains) {
+    // When the domain is set, the cookie is also available to subdomains - otherwise
+    // it is only available on the current domain
+    cookieHeader += `; Domain=${url.hostname};`
+  }
+  headers.append('Set-Cookie', cookieHeader)
+
+  return new Response(undefined, {
+    headers,
+    status: 200,
+  })
+}
+
+export const decodeToken = (token?: string) => {
+  try {
+    if (typeof token !== 'string') {
+      return
+    }
+    // Replace Base64url encoding characters
+    const jwtUrlSafe = token.replaceAll('-', '+').replaceAll('_', '/')
+    const [, payload] = jwtUrlSafe.split('.')
+    const user = atob(payload)
+    return JSON.parse(user) as { exp?: number }
+  } catch {
+    return
+  }
+}
