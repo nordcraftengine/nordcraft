a7d0eab909bcb4185c0cf8ce22c7a898c60bc845
Erik Beuschau <bager.erik@gmail.com>
2025-07-04 08:33:28 +0200

    WIP

--- packages/backend/src/routes/pageHandler.ts
+++ packages/backend/src/routes/pageHandler.ts
@@ -1,33 +1,31 @@
 import { isPageComponent } from '@nordcraft/core/dist/component/isPageComponent'
 import { matchPageForUrl } from '@nordcraft/ssr/dist/routing/routing'
-import type { ProjectFiles } from '@nordcraft/ssr/dist/ssr.types'
 import type { MiddlewareHandler } from 'hono'
 import type { HonoEnv, HonoProject, HonoRoutes } from '../../hono'
-import { loadJsFile } from '../middleware/jsLoader'
+import type { PageLoader } from '../loaders/types'
 import { nordcraftPage } from './nordcraftPage'
 
-export const pageHandler: MiddlewareHandler<
-  HonoEnv<HonoRoutes & HonoProject>
-> = async (ctx, next) => {
-  const url = new URL(ctx.req.url)
-  const page = matchPageForUrl({
-    url,
-    pages: Object.values(ctx.var.routes.pages),
-  })
-  if (page) {
-    const pageContent = await loadJsFile<
-      ProjectFiles & { customCode: boolean }
-    >(`./components/${page.name}.js`)
-    const component = pageContent?.components?.[page.name]
-    if (!component || !isPageComponent(component)) {
-      return next()
-    }
-    return nordcraftPage({
-      hono: ctx,
-      project: ctx.var.project,
-      files: pageContent,
-      page: component,
+export const pageHandler: (
+  pageLoader: PageLoader,
+) => MiddlewareHandler<HonoEnv<HonoRoutes & HonoProject>> =
+  (pageLoader) => async (ctx, next) => {
+    const url = new URL(ctx.req.url)
+    const page = matchPageForUrl({
+      url,
+      pages: Object.values(ctx.var.routes.pages),
     })
+    if (page) {
+      const pageContent = await pageLoader({ env: ctx.env, name: page.name })
+      const component = pageContent?.components?.[page.name]
+      if (!component || !isPageComponent(component)) {
+        return next()
+      }
+      return nordcraftPage({
+        hono: ctx,
+        project: ctx.var.project,
+        files: pageContent,
+        page: component,
+      })
+    }
+    return next()
   }
-  return next()
-}
