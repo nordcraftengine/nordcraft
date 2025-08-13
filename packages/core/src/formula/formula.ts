3dcf05068878f548245be9d0cb05537e0f0d8cda
Mikkel <mikkel@toddle.dev>
2025-08-13 09:03:08 +0200

    fix: specify isFormula type (#504)
    * fix: specify isFormula type
    
    * fix: a couple of types and bump

--- packages/core/src/formula/formula.ts
+++ packages/core/src/formula/formula.ts
@@ -143,7 +143,25 @@ export type ToddleEnv =
     }
 
 export function isFormula(f: any): f is Formula {
-  return f && typeof f === 'object' && typeof f.type === 'string'
+  return (
+    f &&
+    typeof f === 'object' &&
+    typeof f.type === 'string' &&
+    (
+      [
+        'path',
+        'function',
+        'record',
+        'object',
+        'array',
+        'or',
+        'and',
+        'apply',
+        'value',
+        'switch',
+      ] as Formula['type'][]
+    ).includes(f.type)
+  )
 }
 export function isFormulaApplyOperation(
   formula: Formula,
