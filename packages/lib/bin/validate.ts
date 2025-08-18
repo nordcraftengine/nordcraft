b06fc012ec534abd47e9e2b32ecff24d44a3b503
Erik Beuschau <bager.erik@gmail.com>
2025-08-18 10:17:29 +0200

    Add json validation of lib formulas

--- packages/lib/bin/validate.ts
+++ packages/lib/bin/validate.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs'
-import { validate } from 'jsonschema'
+import { type ValidationError, Validator } from 'jsonschema'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
@@ -8,6 +8,21 @@ const __dirname = path.dirname(__filename)
 
 const resolvePath = (...segments: string[]) =>
   path.resolve(__dirname, ...segments)
+
+const validator = new Validator()
+// Let the validator know about the built-in schemas
+const subSchemas = [
+  'formula.schema.json',
+  'valueFormula.schema.json',
+  'pathFormula.schema.json',
+  'arrayFormula.schema.json',
+]
+subSchemas.forEach((schemaFile) => {
+  const schema = JSON.parse(
+    fs.readFileSync(resolvePath(`../schemas/${schemaFile}`), 'utf8'),
+  )
+  validator.addSchema(schema, `/${schema['$id']}`)
+})
 
 const validateJsonFile = (filePath: string) => {
   const file = fs.readFileSync(resolvePath(filePath), 'utf8')
@@ -21,7 +36,7 @@ const validateJsonFile = (filePath: string) => {
     'utf8',
   )
   const schema = JSON.parse(schemaFile)
-  return validate(json, schema)
+  return validator.validate(json, schema)
 }
 
 const actions = fs.readdirSync(resolvePath('../actions'))
@@ -30,6 +45,26 @@ actions.forEach((action) => {
   if (fs.existsSync(resolvePath(actionPath))) {
     const result = validateJsonFile(actionPath)
     if (!result.valid) {
+      // eslint-disable-next-line no-console
+      console.error(
+        `Error validating ${actionPath}:`,
+        result.errors.map((e: ValidationError) => e.message),
+      )
+      process.exit(1)
+    }
+  }
+})
+const formulas = fs.readdirSync(resolvePath('../formulas'))
+formulas.forEach((formula) => {
+  const formulaPath = `../formulas/${formula}/formula.json`
+  if (fs.existsSync(resolvePath(formulaPath))) {
+    const result = validateJsonFile(formulaPath)
+    if (!result.valid) {
+      // eslint-disable-next-line no-console
+      console.error(
+        `Error validating ${formulaPath}:`,
+        result.errors.map((e: ValidationError) => e.message),
+      )
       process.exit(1)
     }
   }
