5047837767c2109504c2bde57c0a19e664f4daf5
Erik Beuschau <32451595+erik-beus@users.noreply.github.com>
2025-09-05 10:47:46 +0200

    Improve test data removal from actions (#535)

--- packages/ssr/src/rendering/testData.ts
+++ packages/ssr/src/rendering/testData.ts
@@ -1,69 +1,110 @@
 import { isLegacyApi } from '@nordcraft/core/dist/api/api'
-import type { Component } from '@nordcraft/core/dist/component/component.types'
-import {
-  mapObject,
-  omit,
-  omitKeys,
-} from '@nordcraft/core/dist/utils/collections'
+import type {
+  ActionModel,
+  Component,
+  CustomActionArgument,
+  NodeModel,
+} from '@nordcraft/core/dist/component/component.types'
+import { mapObject, omitKeys } from '@nordcraft/core/dist/utils/collections'
+import { isDefined } from '@nordcraft/core/dist/utils/util'
 
-export function removeTestData(component: Component): Component {
-  return {
-    ...component,
-    attributes: mapObject(component.attributes, ([key, value]) => [
-      key,
-      omit(value, ['testValue']),
-    ]),
-    ...(component.route
-      ? {
-          route: {
-            ...component.route,
-            path: component.route.path.map((p) => omit(p, ['testValue'])),
-            query: mapObject(component.route.query, ([key, value]) => [
-              key,
-              omit(value, ['testValue']),
-            ]),
+export const removeTestData = (component: Component): Component => ({
+  ...component,
+  attributes: mapObject(component.attributes, ([key, value]) => [
+    key,
+    omitKeys(value, ['testValue']),
+  ]),
+  nodes: mapObject(component.nodes, ([key, value]) => {
+    if ('events' in value && isDefined(value.events)) {
+      const updatedNode: NodeModel = {
+        ...value,
+        events: mapObject(value.events, ([eventKey, eventValue]) => [
+          eventKey,
+          {
+            ...eventValue,
+            actions: eventValue.actions.map(removeActionTestData),
           },
-        }
-      : {}),
-    ...(component.formulas
-      ? {
-          formulas: mapObject(component.formulas, ([key, value]) => [
+        ]),
+      }
+      return [key, updatedNode as NodeModel]
+    }
+    return [key, value]
+  }),
+  ...(component.route
+    ? {
+        route: {
+          ...component.route,
+          path: component.route.path.map((p) => omitKeys(p, ['testValue'])),
+          query: mapObject(component.route.query, ([key, value]) => [
             key,
-            {
-              ...value,
-              // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-              arguments: (value.arguments ?? []).map((a) =>
-                omit(a, ['testValue']),
-              ),
-            },
+            omitKeys(value, ['testValue']),
           ]),
-        }
-      : {}),
-    ...(component.workflows
-      ? {
-          workflows: mapObject(component.workflows, ([key, value]) => [
-            key,
-            {
-              ...value,
-              // We should find all actions (also nested actions and non-workflow actions) and remove
-              // the description from them. This is a start though
-              actions: value.actions.map((a) =>
-                a.type === 'Custom' ? omitKeys(a, ['description']) : a,
-              ),
-              // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-              parameters: (value.parameters || []).map((p) =>
-                omit(p, ['testValue']),
-              ),
-            },
-          ]),
-        }
-      : {}),
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    apis: mapObject(component.apis ?? {}, ([key, api]) => [
-      key,
-      // service and servicePath are only necessary in the editor. All information about an API
-      // request is available on the api object itself
-      isLegacyApi(api) ? api : omitKeys(api, ['service', 'servicePath']),
-    ]),
+        },
+      }
+    : {}),
+  ...(component.formulas
+    ? {
+        formulas: mapObject(component.formulas, ([key, value]) => [
+          key,
+          {
+            ...value,
+            arguments: (value.arguments ?? []).map((a) =>
+              omitKeys(a, ['testValue']),
+            ),
+          },
+        ]),
+      }
+    : {}),
+  ...(component.workflows
+    ? {
+        workflows: mapObject(component.workflows, ([key, value]) => [
+          key,
+          {
+            ...value,
+            // We should find all actions (also nested actions and non-workflow actions) and remove
+            // the description from them. This is a start though
+            actions: value.actions.map(removeActionTestData),
+            // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+            parameters: (value.parameters || []).map((p) =>
+              omitKeys(p, ['testValue']),
+            ),
+          },
+        ]),
+      }
+    : {}),
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  apis: mapObject(component.apis ?? {}, ([key, api]) => [
+    key,
+    // service and servicePath are only necessary in the editor. All information about an API
+    // request is available on the api object itself
+    isLegacyApi(api) ? api : omitKeys(api, ['service', 'servicePath']),
+  ]),
+  onLoad: component.onLoad
+    ? {
+        ...component.onLoad,
+        actions: component.onLoad.actions.map(removeActionTestData),
+      }
+    : undefined,
+  onAttributeChange: component.onAttributeChange
+    ? {
+        ...component.onAttributeChange,
+        actions: component.onAttributeChange.actions.map(removeActionTestData),
+      }
+    : undefined,
+})
+
+const removeActionTestData = (action: ActionModel) => {
+  if (action.type === 'Custom') {
+    return {
+      // The 'group' property was used for categorizing actions and should
+      // not have been persisted in projects. But since it's there (for some actions)
+      // we should remove it
+      ...omitKeys(action, ['description', 'group', 'label']),
+      arguments: action.arguments?.map(removeActionArgumentTestData),
+    }
   }
+  return action
 }
+
+const removeActionArgumentTestData = (action: CustomActionArgument) =>
+  omitKeys(action, ['description', 'type'])
