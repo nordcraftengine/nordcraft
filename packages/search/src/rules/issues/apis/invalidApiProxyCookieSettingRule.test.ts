f20dfabf183febe471b527d874f0c13e9e5ee9d0
Erik Beuschau <32451595+erik-beus@users.noreply.github.com>
2025-10-07 10:24:26 +0200

    Add invalid proxy+cookie setting rule (#603)

---
+++ packages/search/src/rules/issues/apis/invalidApiProxyCookieSettingRule.test.ts
@@ -0,0 +1,132 @@
+import { valueFormula } from '@nordcraft/core/dist/formula/formulaUtils'
+import { describe, expect, test } from 'bun:test'
+import { searchProject } from '../../../searchProject'
+import { invalidApiProxyCookieSettingRule } from './invalidApiProxyCookieSettingRule'
+
+describe('invalidApiProxyCookieSettingRule', () => {
+  test('should report invalid usage of GetHttpOnlyCookie when the API is not proxied', () => {
+    const problems = Array.from(
+      searchProject({
+        files: {
+          components: {
+            test: {
+              name: 'test',
+              nodes: {},
+              formulas: {},
+              apis: {
+                'my-api': {
+                  name: 'my-api',
+                  type: 'http',
+                  version: 2,
+                  autoFetch: valueFormula(true),
+                  inputs: {},
+                  headers: {
+                    Authorization: {
+                      formula: {
+                        type: 'function',
+                        name: '@toddle/concatenate',
+                        arguments: [
+                          { formula: { type: 'value', value: 'Bearer ' } },
+                          {
+                            formula: {
+                              type: 'function',
+                              name: '@toddle/getHttpOnlyCookie',
+                              arguments: [
+                                {
+                                  formula: {
+                                    type: 'value',
+                                    value: 'access_token',
+                                  },
+                                },
+                              ],
+                              display_name: 'Get Http-Only Cookie',
+                            },
+                          },
+                        ],
+                        display_name: 'Concatenate',
+                      },
+                    },
+                  },
+                  server: {
+                    proxy: {
+                      enabled: { formula: valueFormula(true) },
+                    },
+                  },
+                },
+              },
+              attributes: {},
+              variables: {},
+            },
+          },
+        },
+        rules: [invalidApiProxyCookieSettingRule],
+      }),
+    )
+
+    expect(problems).toHaveLength(1)
+    expect(problems[0].code).toBe('invalid api proxy body setting')
+    expect(problems[0].details).toEqual({ api: 'my-api' })
+  })
+
+  test('should not report valid usage of GetHttpOnlyCookie when API is proxied', () => {
+    const problems = Array.from(
+      searchProject({
+        files: {
+          components: {
+            test: {
+              name: 'test',
+              nodes: {},
+              formulas: {},
+              apis: {
+                'my-api': {
+                  name: 'my-api',
+                  type: 'http',
+                  version: 2,
+                  autoFetch: valueFormula(true),
+                  inputs: {},
+                  headers: {
+                    Authorization: {
+                      formula: {
+                        type: 'function',
+                        name: '@toddle/concatenate',
+                        arguments: [
+                          { formula: { type: 'value', value: 'Bearer ' } },
+                          {
+                            formula: {
+                              type: 'function',
+                              name: '@toddle/getHttpOnlyCookie',
+                              arguments: [
+                                {
+                                  formula: {
+                                    type: 'value',
+                                    value: 'access_token',
+                                  },
+                                },
+                              ],
+                              display_name: 'Get Http-Only Cookie',
+                            },
+                          },
+                        ],
+                        display_name: 'Concatenate',
+                      },
+                    },
+                  },
+                  server: {
+                    proxy: {
+                      enabled: { formula: valueFormula(false) },
+                    },
+                  },
+                },
+              },
+              attributes: {},
+              variables: {},
+            },
+          },
+        },
+        rules: [invalidApiProxyCookieSettingRule],
+      }),
+    )
+
+    expect(problems).toEqual([])
+  })
+})
