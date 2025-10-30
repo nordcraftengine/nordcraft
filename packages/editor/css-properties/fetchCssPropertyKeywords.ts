8219b41e99c590e0ffae282e9f4eb491f27c5b50
Mikkel <mikkel@toddle.dev>
2025-10-30 11:48:35 +0100

    fix(css-properties): use mdn data, w3c data is bad

---
+++ packages/editor/css-properties/fetchCssPropertyKeywords.ts
@@ -0,0 +1,71 @@
+import { isDefined } from '@nordcraft/core/src/utils/util'
+import { definitionSyntax } from 'css-tree'
+import { css } from 'mdn-data'
+
+const GLOBAL_KEYWORDS = [
+  'inherit',
+  'initial',
+  'unset',
+  'revert',
+  'revert-layer',
+]
+
+/**
+ * Generates a JSON map of all CSS properties → supported keyword values,
+ * including global CSS keywords (inherit, initial, unset, revert, revert-layer).
+ *
+ * Uses MDN data. MDN data is in the process of being deprecated in favor of w3c/webref.
+ * I made an attempt at using the data on w3c/webref, but there are currently too many issues (as of 30/10/2025).
+ */
+export function getCssKeywordsMap() {
+  const { properties, syntaxes } = css
+
+  try {
+    const recursiveWalk = (syntax: string, keywords: Set<string>) => {
+      definitionSyntax.walk(definitionSyntax.parse(syntax), (node) => {
+        if (
+          node.type === 'Type' &&
+          // exclude functions
+          !node.name.includes('()') &&
+          // exclude named colors
+          !node.name.includes('color')
+        ) {
+          const type = syntaxes[node.name]
+
+          if (isDefined(type)) {
+            recursiveWalk(type.syntax, keywords)
+          }
+        }
+
+        if (node.type === 'Keyword') {
+          keywords.add(node.name)
+        }
+      })
+    }
+
+    return Object.entries(properties).reduce((result, [name, data]) => {
+      const keywordSet: Set<string> = new Set()
+
+      if (data.syntax) {
+        try {
+          recursiveWalk(data.syntax, keywordSet)
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.warn(
+            `Error detected in the syntax of the ${name} property. Parsing of property skipped.`,
+            error,
+          )
+        }
+      }
+
+      return {
+        ...result,
+        [name]: Array.from(keywordSet).concat(GLOBAL_KEYWORDS),
+      }
+    }, {})
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error)
+    process.exit(1)
+  }
+}
