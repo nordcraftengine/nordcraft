ea6c783d9873897f644a033d2261e50984e67b83
Erik Beuschau <bager.erik@gmail.com>
2025-11-11 11:59:02 +0100

    Enable noUncheckedIndexedAccess for core package

--- packages/core/src/api/LegacyToddleApi.ts
+++ packages/core/src/api/LegacyToddleApi.ts
@@ -36,7 +36,10 @@ export class LegacyToddleApi<Handler> {
       switch (formula.type) {
         case 'path':
           if (formula.path[0] === 'Apis') {
-            apis.add(formula.path[1])
+            const apiName = formula.path[1]
+            if (typeof apiName === 'string') {
+              apis.add(apiName)
+            }
           }
           break
         case 'value':
