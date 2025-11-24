4468f8ee799395f1ecf2fe3cd6e8008038ec5100
Erik Beuschau <32451595+erik-beus@users.noreply.github.com>
2025-11-24 10:47:05 +0100

    Exclude irrelevant HTML attributes (#680)

--- packages/editor/elements/_dev_buildInterfaces.ts
+++ packages/editor/elements/_dev_buildInterfaces.ts
@@ -5,6 +5,27 @@ import { writeFileSync } from 'fs'
 // mapping HTML interfaces to their attributes.
 // The file is saved as ./interfaces/htmlInterfaceAttributes.json
 
+interface HtmlDefinition {
+  id: string
+  href: string
+  linkingText: string[]
+  localLinkingText: string[]
+  type: 'attribute' | 'element-attr' | 'dfn' // We only care about attributes and element-attr
+  for: string[]
+  access: 'public' | 'private' // Not relevant
+  informative: boolean // Not relevant
+  heading: any // Not relevant
+  definedIn: string // Not relevant
+  links: [] // Not relevant
+}
+
+interface HtmlInterfaceDefinition {
+  attributes?: string[]
+  events?: string[]
+}
+
+const EXCLUDED_ATTRIBUTE_NAMES = new Set(['innerHTML', 'outerHTML'])
+
 const mapInterfaceName = (name: string) => {
   const interfaceMapping: Partial<Record<string, string>> = {
     'html-global': 'global',
@@ -14,88 +35,85 @@ const mapInterfaceName = (name: string) => {
 
 const definitions: {
   spec: { title: string; url: string }
-  dfns: Array<{
-    id: string
-    href: string
-    linkingText: string[]
-    localLinkingText: []
-    type: 'attribute' | 'element-attr' | 'dfn' // We only care about attributes and element-attr
-    for: string[]
-    access: 'public' | 'private' // Not relevant
-    informative: boolean // Not relevant
-    heading: any // Not relevant
-    definedIn: string // Not relevant
-    links: [] // Not relevant
-  }>
+  dfns: HtmlDefinition[]
 } = await fetch(
   'https://raw.githubusercontent.com/w3c/webref/refs/heads/main/ed/dfns/html.json',
 ).then((res) => res.json())
 const groupedAttributes = definitions.dfns
   .filter(
-    (d) =>
-      // Ignore all event attributes
-      !d.id.startsWith('handler-') &&
-      // Only keep attributes and element-attr definitions (if they're global)
-      (d.type === 'attribute' ||
+    // Make Typescript aware that linkingText[0] exists if the filter is truthy
+    (d): d is HtmlDefinition & { linkingText: [string, ...string[]] } => {
+      const attrName = d.linkingText[0]
+      return (
+        // Ignore all event attributes
+        (!d.id.startsWith('handler-') &&
+          d.linkingText.length > 0 &&
+          typeof attrName === 'string' &&
+          // Only include relevant attributes
+          !EXCLUDED_ATTRIBUTE_NAMES.has(attrName) &&
+          // Skip internal attributes
+          !(attrName.startsWith('[[') && attrName.endsWith(']]')) &&
+          // Only keep attributes and element-attr definitions (if they're global)
+          d.type === 'attribute') ||
         (d.type === 'element-attr' &&
-          (d.for.includes('html-global') || d.for.includes('global')))),
+          (d.for.includes('html-global') || d.for.includes('global')))
+      )
+    },
   )
   .reduce(
     (acc, d) => {
       const attrName = d.linkingText[0]
-      if (typeof attrName !== 'string') {
-        throw new Error('Invalid attribute name for dfn: ' + JSON.stringify(d))
-      }
-      if (attrName.startsWith('[[') && attrName.endsWith(']]')) {
-        return acc // Skip internal attributes
-      }
       const interfaceNames = d.for
       interfaceNames.forEach((_interfaceName) => {
         const interfaceName = mapInterfaceName(_interfaceName)
-        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-        if (!acc[interfaceName]) {
-          acc[interfaceName] = {
-            // The 'href' attribute is currently missing in the webref data for the HTMLAnchorElement interface
-            attributes: interfaceName === 'HTMLAnchorElement' ? ['href'] : [],
-          }
-        }
+        acc[interfaceName] ??= {}
+        // The 'href' attribute is currently missing in the webref data for the HTMLAnchorElement interface
+        // So we add it manually here
+        acc[interfaceName].attributes ??=
+          interfaceName === 'HTMLAnchorElement' ? ['href'] : []
         acc[interfaceName].attributes.push(attrName)
       })
       return acc
     },
-    {} as Record<string, { attributes: string[]; events?: string[] }>,
+    {} as Partial<Record<string, HtmlInterfaceDefinition>>,
   )
 
 // Add events
 const events = await listAllEvents()
 events.forEach((e) => {
   e.targets.forEach((et) => {
-    const interfaceItem = groupedAttributes[et.target]
-    if (interfaceItem) {
-      // Only add events if the interface is already present
-      // Later, we might want to add events for SVGElement, MathMLElement, etc.
-      interfaceItem.events ??= []
-      interfaceItem.events.push(e.type)
+    let interfaceItem = groupedAttributes[et.target]
+    if (!interfaceItem) {
+      interfaceItem = { attributes: [] }
+      groupedAttributes[et.target] = interfaceItem
     }
+    interfaceItem.events ??= []
+    interfaceItem.events.push(e.type)
   })
 })
 
+// Sort all interfaces and write to file
 writeFileSync(
   `./interfaces/htmlInterfaces.json`,
   JSON.stringify(
-    Object.entries(groupedAttributes).reduce<
-      Array<{ name: string; attributes: string[] }>
-    >(
-      (acc, [interfaceName, data]) => [
-        ...acc,
-        {
-          name: interfaceName,
-          attributes: data.attributes.toSorted(),
-          events: data.events?.toSorted(),
-        },
-      ],
-      [],
-    ),
+    Object.entries(groupedAttributes)
+      .toSorted(([a], [b]) => a.localeCompare(b))
+      // To help Typescript narrow the type
+      .filter(
+        (args): args is [string, HtmlInterfaceDefinition] =>
+          args[1] !== undefined,
+      )
+      .reduce<Array<HtmlInterfaceDefinition & { name: string }>>(
+        (acc, [interfaceName, data]) => [
+          ...acc,
+          {
+            name: interfaceName,
+            attributes: data.attributes?.toSorted(),
+            events: data.events?.toSorted(),
+          },
+        ],
+        [],
+      ),
     null,
     2,
   ),
