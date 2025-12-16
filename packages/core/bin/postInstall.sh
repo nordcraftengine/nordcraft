41eff6b8b34a61c06629cb226a54cf482150230e
Erik Beuschau <bager.erik@gmail.com>
2025-12-16 11:38:19 +0100

    Adjust postinstall script

--- packages/core/bin/postInstall.sh
+++ packages/core/bin/postInstall.sh
@@ -1,7 +1,7 @@
+echo "Running post install script for core package"
 if [ "$NODE_ENV" = "production" ]; then
   echo "Skipping post install script in production environment"
   exit 0
 fi
-echo "Running post install script for core package"
 tsgo -p ./tsconfig.install.json
-bun run ./bin/generateSchemas.ts
+bun run generateTypes
