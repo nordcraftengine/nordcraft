00c88c63d161a8f6e814e9384a60d3bb0193fc9a
Erik Beuschau <bager.erik@gmail.com>
2025-12-16 11:48:26 +0100

    Adjust Dockerfile

--- packages/core/bin/postInstall.sh
+++
@@ -1,7 +0,0 @@
-echo "Running post install script for core package"
-if [ "$NODE_ENV" = "production" ]; then
-  echo "Skipping post install script in production environment"
-  exit 0
-fi
-tsgo -p ./tsconfig.install.json
-bun run generateTypes
