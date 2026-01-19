3faaa7699def50e6d0841de01f664d4d12205cca
Erik Beuschau <bager.erik@gmail.com>
2026-01-19 10:38:45 +0100

    Ensure the zod schema matches component.types

--- packages/core/src/component/schemas/action-schema.ts
+++ packages/core/src/component/schemas/action-schema.ts
@@ -47,6 +47,7 @@ const SwitchActionModelSchema: z.ZodType<SwitchActionModel> = z
             .describe('List of actions to execute if the condition is met.'),
         }),
       )
+      .nullish()
       .describe(
         'Cases for the switch action. Each case has a condition and actions.',
       ),
@@ -54,6 +55,7 @@ const SwitchActionModelSchema: z.ZodType<SwitchActionModel> = z
       .object({
         actions: z.array(z.lazy(() => ActionModelSchema)),
       })
+      .nullish()
       .describe('Actions to execute if no case conditions are met.'),
   })
   .describe(
