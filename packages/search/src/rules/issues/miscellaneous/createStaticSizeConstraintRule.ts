04ee454da70de02724c19f410e9750eba4794fd7
Erik Beuschau <32451595+erik-beus@users.noreply.github.com>
2026-02-12 12:18:51 +0100

    Add staticSizeConstraint rule (#115)

---
+++ packages/search/src/rules/issues/miscellaneous/createStaticSizeConstraintRule.ts
@@ -0,0 +1,90 @@
+import type { NodeModel } from '@nordcraft/core/dist/component/component.types'
+import { isDefined } from '@nordcraft/core/dist/utils/util'
+import { VOID_HTML_ELEMENTS } from '@nordcraft/ssr/dist/const'
+import type { Level, Rule } from '../../../types'
+
+export function createStaticSizeConstraintRule(
+  tag: string,
+  maxSize: number,
+  level: Level = 'info',
+): Rule<{
+  tag: string
+  size: number
+}> {
+  return {
+    code: 'size constraint',
+    category: 'Performance',
+    level: level,
+    visit: (report, args) => {
+      if (
+        args.nodeType === 'component-node' &&
+        args.value.type === 'element' &&
+        args.value.tag === tag
+      ) {
+        let size = 0
+        const component = args.component
+        const evaluateElement = (element?: NodeModel): string => {
+          if (
+            !element ||
+            ['element', 'text', 'slot', 'component'].includes(element.type) ===
+              false
+          ) {
+            return ''
+          }
+          const children = (element.children ?? []).map((child) =>
+            evaluateElement(component.nodes?.[child]),
+          )
+          const tag = element.type === 'element' ? element.tag : 'span'
+          const attributes: Record<string, string> = {}
+          if (element.type === 'element' || element.type === 'component') {
+            Object.entries(element.attrs ?? {}).forEach(([key, value]) => {
+              if (value?.type === 'value' && isDefined(value.value)) {
+                attributes[key] = String(value.value)
+              }
+            })
+          }
+          const attributeString = Object.entries(attributes)
+            .map(([key, value]) => `${key}="${value}"`)
+            .join(' ')
+          if (VOID_HTML_ELEMENTS.includes(tag)) {
+            return attributeString
+              ? `<${tag} ${attributeString} />`
+              : `<${tag} />`
+          } else {
+            const openingTag = attributeString
+              ? `<${tag} ${attributeString}>`
+              : `<${tag}>`
+            return `${openingTag}${children.join('')}</${tag}>`
+          }
+        }
+        const staticElement = evaluateElement(args.value)
+        size = new Blob([staticElement]).size
+        if (size > maxSize) {
+          report({
+            path: args.path,
+            info: {
+              title: `Element size exceeds the suggested maximum allowed size of ${sizeFormatter(maxSize)}`,
+              description: `The <${tag}> element has a size of ${sizeFormatter(size)}, which exceeds the suggested limit of ${sizeFormatter(maxSize)}. Consider simplifying the content or structure of this element to reduce its size and improve performance.`,
+            },
+            details: { tag, size },
+          })
+        }
+      }
+    },
+  }
+}
+
+const sizeFormatter = (sizeInBytes: number) => {
+  const units = ['byte', 'KB', 'MB', 'GB', 'TB']
+  let unitIndex = 0
+  let size = sizeInBytes
+
+  while (size >= 1024 && unitIndex < units.length - 1) {
+    size /= 1024
+    unitIndex++
+  }
+  const unit = unitIndex === 0 && size !== 1 ? 'bytes' : units[unitIndex]
+  return `${new Intl.NumberFormat(undefined, {
+    maximumFractionDigits: unitIndex === 0 ? 0 : 2,
+  }).format(size)} ${unit}`
+}
--- packages/search/src/rules/issues/miscellaneous/miscRules.index.ts
+++ packages/search/src/rules/issues/miscellaneous/miscRules.index.ts
@@ -1,5 +1,14 @@
+import { createStaticSizeConstraintRule } from './createStaticSizeConstraintRule'
 import { noReferenceNodeRule } from './noReferenceNodeRule'
 import { requireExtensionRule } from './requireExtensionRule'
 import { unknownCookieRule } from './unknownCookieRule'
 
-export default [noReferenceNodeRule, requireExtensionRule, unknownCookieRule]
+export default [
+  noReferenceNodeRule,
+  requireExtensionRule,
+  unknownCookieRule,
+  // 100 KB is a large SVG
+  createStaticSizeConstraintRule('svg', 100 * 1024),
+  // 50 KB is a large img element (with potential base64 encoded image)
+  createStaticSizeConstraintRule('img', 50 * 1024),
+]
