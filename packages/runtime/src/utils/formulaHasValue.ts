06b87cbb391438abea4ad2de205640baa45718f5
Jacob Kofoed <jacob.m.kofoed@gmail.com>
2026-02-18 13:27:18 +0100

    amend

--- packages/runtime/src/utils/formulaHasValue.ts
+++ packages/runtime/src/utils/formulaHasValue.ts
@@ -1,5 +1,7 @@
 import type { Formula } from '@nordcraft/core/dist/formula/formula'
-import { isDefined } from '@nordcraft/core/dist/src/utils/util'
+import { isDefined } from '@nordcraft/core/dist/utils/util'
 
-export const formulaHasValue = (formula: Formula | undefined) =>
+export const formulaHasValue = (
+  formula: Formula | undefined,
+): formula is Formula =>
   isDefined(formula) && !(formula.type === 'value' && !isDefined(formula.value))
