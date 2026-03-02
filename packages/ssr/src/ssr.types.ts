e800575ace862cc32b11a8d8695151a5f3c2f8d3
Christian Esbensen <christian@toddle.dev>
2026-03-02 14:26:33 +0100

    Added missing services types

--- packages/ssr/src/ssr.types.ts
+++ packages/ssr/src/ssr.types.ts
@@ -71,11 +71,29 @@ interface XanoApiService extends BaseApiService {
   type: 'xano'
 }
 
+interface DatoCmsApiService extends BaseApiService {
+  type: 'datocms'
+}
+
+interface UmbracoApiService extends BaseApiService {
+  type: 'umbraco'
+}
+
+interface ContentfulApiService extends BaseApiService {
+  type: 'contentful'
+}
+
 interface CustomApiService extends BaseApiService {
   type: 'custom'
 }
 
-export type ApiService = SupabaseApiService | XanoApiService | CustomApiService
+export type ApiService =
+  | SupabaseApiService
+  | XanoApiService
+  | DatoCmsApiService
+  | UmbracoApiService
+  | ContentfulApiService
+  | CustomApiService
 
 export type InstalledPackage = Pick<
   ProjectFiles,
