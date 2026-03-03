d16bfa22244e77ac4e19726637868dda33db0ff2
Jacob Kofoed <jacob.m.kofoed@gmail.com>
2026-03-03 12:01:50 +0100

    Allow disabling date parsing for performance-gains (#799)

--- packages/lib/formulas/parseJSON/handler.ts
+++ packages/lib/formulas/parseJSON/handler.ts
@@ -1,13 +1,17 @@
 import type { FormulaHandler } from '@nordcraft/core/dist/types'
 import { parseJSONWithDate } from '@nordcraft/core/dist/utils/json'
 
-const handler: FormulaHandler<unknown> = ([data]) => {
+const handler: FormulaHandler<unknown> = ([data, parseDate]) => {
   if (typeof data !== 'string') {
     return null
   }
+
   try {
     // Similar to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
-    return parseJSONWithDate(data)
+    if (parseDate === null || parseDate === undefined || Boolean(parseDate)) {
+      return parseJSONWithDate(data)
+    }
+    return JSON.parse(data)
   } catch {
     return null
   }
