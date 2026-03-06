543643d3f27b38d2014aefe554e681ed10f2cb3d
Jacob Kofoed <jacob.m.kofoed@gmail.com>
2026-03-06 13:54:06 +0100

    Escape backslash in class selectors (#804)
    * Escape backslash in class selectors
    
    * Escape SSR also
    
    * bump

--- packages/runtime/src/styles/CustomPropertyStyleSheet.test.ts
+++ packages/runtime/src/styles/CustomPropertyStyleSheet.test.ts
@@ -151,4 +151,28 @@ describe('CustomPropertyStyleSheet', () => {
     )
     expect(instance.getStyleSheet().cssRules).toBeEmpty()
   })
+
+  test('it escapes backslashes in selectors', () => {
+    const instance = new CustomPropertyStyleSheet(document)
+    instance.registerProperty(
+      '[data-id].my-package/my-component:hover',
+      '--my-property',
+    )('value')
+    expect(instance.getStyleSheet().cssRules.length).toBe(1)
+    expect(instance.getStyleSheet().cssRules[0].cssText).toBe(
+      '[data-id].my-package\\/my-component:hover { --my-property: value; }',
+    )
+  })
+
+  test('it does not escape backslashes that are already escaped in selectors', () => {
+    const instance = new CustomPropertyStyleSheet(document)
+    instance.registerProperty(
+      '[data-id].my-package\\/my-component:hover',
+      '--my-property',
+    )('value')
+    expect(instance.getStyleSheet().cssRules.length).toBe(1)
+    expect(instance.getStyleSheet().cssRules[0].cssText).toBe(
+      '[data-id].my-package\\/my-component:hover { --my-property: value; }',
+    )
+  })
 })
