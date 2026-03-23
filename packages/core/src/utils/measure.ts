80d5ac120c08acfc5cac27e55bdbffe7aeeb7cd9
Jacob Kofoed <jacob.m.kofoed@gmail.com>
2026-03-23 11:52:01 +0100

    Helper function to enable/disable measuring

--- packages/core/src/utils/measure.ts
+++ packages/core/src/utils/measure.ts
@@ -1,50 +1,59 @@
-const MAX_STACK_SIZE = 10
-let stackIndex = 0
-const stack: {
-  key: string
-  details: Record<string, unknown>
-  start: number
-}[] = []
+const globalScope: any = typeof globalThis !== 'undefined' ? globalThis : window
+globalScope.__nc_measure_max_depth = 10
+globalScope.__nc_measure_enabled =
+  typeof sessionStorage !== 'undefined' &&
+  sessionStorage.getItem('__nc_measure') === 'true'
+
+globalScope.__nc_enableMeasure = (enabled = true, maxDepth = 10) => {
+  if (enabled) {
+    sessionStorage.setItem('__nc_measure', 'true')
+  } else {
+    sessionStorage.removeItem('__nc_measure')
+  }
+  globalScope.__nc_measure_enabled = enabled
+  globalScope.__nc_measure_max_depth = maxDepth
+}
+
+let measureCount = 0
+const STACK: string[] = []
+const NOOP = () => {}
+
 export const measure = (key: string, details: Record<string, unknown>) => {
-  if (
-    typeof sessionStorage === 'undefined' ||
-    sessionStorage.getItem('__nc_measure') !== 'true'
-  ) {
-    return () => {}
+  if (!globalScope.__nc_measure_enabled) {
+    return NOOP
   }
 
-  const selfIndex = stackIndex++
-  if (stack.length >= MAX_STACK_SIZE) {
-    return () => {}
+  const selfIndex = measureCount++
+  if (STACK.length >= globalScope.__nc_measure_max_depth) {
+    return NOOP
   }
 
-  const stackEntry = {
-    key,
-    details,
-    start: performance.now(),
-  }
-  stack.push(stackEntry)
+  const start = performance.now()
+  STACK.push(key)
+
   return (extraDetails?: Record<string, unknown>) => {
-    performance.measure(stackEntry.key, {
-      start: stackEntry.start,
-      end: performance.now(),
+    const end = performance.now()
+    const mergedDetails = extraDetails
+      ? { ...details, ...extraDetails }
+      : details
+
+    performance.measure(key, {
+      start,
+      end,
       detail: {
         devtools: {
           dataType: 'track-entry',
           track: 'Nordcraft devtools',
           properties: [
-            ...Object.entries({ ...details, ...extraDetails }).map(([k, v]) => [
-              k,
-              String(v),
-            ]),
-            ['Stack depth', stack.length],
+            ...Object.entries(mergedDetails).map(([k, v]) => [k, String(v)]),
+            ['Stack', STACK.join(' > ')],
             ['Measure index', selfIndex],
-            ['Sub-measures', stackIndex - selfIndex],
+            ['Sub-measures', measureCount - selfIndex],
           ],
           tooltipText: `${selfIndex}. ${key}`,
         },
       },
     })
-    stack.pop()
+    STACK.pop()
   }
 }
