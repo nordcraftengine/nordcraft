debcbb7a3d5b6d3688e930d2d2d50dbdefda42bf
Mikkel <mikkel@toddle.dev>
2026-03-24 10:19:43 +0100

    feat(focus): support preventScroll option

--- packages/lib/actions/focus/handler.ts
+++ packages/lib/actions/focus/handler.ts
@@ -1,8 +1,14 @@
 import type { ActionHandler } from '@nordcraft/core/dist/types'
 
-const handler: ActionHandler = ([elem]) => {
+const handler: ActionHandler = ([elem, preventScroll]) => {
   if (elem instanceof HTMLElement) {
-    elem.focus()
+    if (typeof preventScroll === 'boolean') {
+      elem.focus({
+        preventScroll,
+      })
+    } else {
+      elem.focus()
+    }
   }
 }
 
