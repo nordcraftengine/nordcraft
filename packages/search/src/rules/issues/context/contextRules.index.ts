c79d0c98575f538a106d6774f4cceef3c90a58c9
Erik Beuschau <bager.erik@gmail.com>
2026-05-08 09:35:17 +0200

    Revert "Improved and added rules for context..."
    This reverts commit d458f1e9975ff16d95a3816aaac50d6272e73b98.

--- packages/search/src/rules/issues/context/contextRules.index.ts
+++ packages/search/src/rules/issues/context/contextRules.index.ts
@@ -1,16 +1,12 @@
 import { unknownContextProviderWorkflowRule } from '../workflows/unknownContextProviderWorkflowRule'
 import { unknownContextWorkflowRule } from '../workflows/unknownContextWorkflowRule'
 import { noContextConsumersRule } from './noContextConsumersRule'
-import { noReferenceContextFormulaRule } from './noReferenceContextFormulaRule'
-import { noReferenceContextWorkflowRule } from './noReferenceContextWorkflowRule'
 import { unknownContextFormulaRule } from './unknownContextFormulaRule'
 import { unknownContextProviderFormulaRule } from './unknownContextProviderFormulaRule'
 import { unknownContextProviderRule } from './unknownContextProviderRule'
 
 export default [
   noContextConsumersRule,
-  noReferenceContextFormulaRule,
-  noReferenceContextWorkflowRule,
   unknownContextFormulaRule,
   unknownContextProviderFormulaRule,
   unknownContextProviderRule,
