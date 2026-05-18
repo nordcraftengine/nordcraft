2f64542a788c7825af8f620268ade3da78ae1b52
Erik Beuschau <bager.erik@gmail.com>
2026-05-18 14:37:34 +0200

    Bump esbuild and revert build changes

--- packages/runtime/scripts/build.js
+++ packages/runtime/scripts/build.js
@@ -3,7 +3,7 @@ const esbuild = require('esbuild')
 // In order to serve page.main.js + custom-element.main.js as ES modules, it's useful if
 // we build them as part of the runtime package. This way, we can import them from other
 // packages without having to worry about the build process.
-const output = esbuild.build({
+esbuild.build({
   entryPoints: ['src/page.main.ts', 'src/custom-element.main.ts'],
   bundle: true,
   sourcemap: true,
@@ -12,19 +12,4 @@ const output = esbuild.build({
   outdir: 'dist',
   format: 'esm',
   entryNames: '[dir]/[name].esm',
-  metafile: true,
 })
-// Write the metafile
-output
-  .then((result) => {
-    const metafilePath = 'dist/meta.json'
-    require('fs').writeFileSync(
-      metafilePath,
-      JSON.stringify(result.metafile, null, 2),
-    )
-    console.log(`Metafile written to ${metafilePath}`)
-  })
-  .catch((error) => {
-    console.error('Build failed:', error)
-    process.exit(1)
-  })
