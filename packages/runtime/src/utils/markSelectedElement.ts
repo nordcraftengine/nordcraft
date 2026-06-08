610f6ab51638d6427aff908d892dd606a9d8c73d
Marija Gerasimovska <marija@toddle.dev>
2026-06-08 14:07:19 +0200

    Add an attribute to the selected repeating items

--- packages/runtime/src/utils/markSelectedElement.ts
+++ packages/runtime/src/utils/markSelectedElement.ts
@@ -3,6 +3,16 @@ export function markSelectedElement(node: Element | null) {
     document.querySelectorAll('[data-selected="true"]').forEach((el) => {
       el.removeAttribute('data-selected')
     })
+    document.querySelectorAll('[data-repeat-selected="true"]').forEach((el) => {
+      el.removeAttribute('data-repeat-selected')
+    })
+
     node.setAttribute('data-selected', 'true')
+
+    const dataId = node.getAttribute('data-id')
+
+    document.querySelectorAll(`[data-id^="${dataId}("]`).forEach((el) => {
+      el.setAttribute('data-repeat-selected', 'true')
+    })
   }
 }
