29ef5660ff4363e6df038fb0c83f2b3295b0d826
Erik Beuschau <32451595+erik-beus@users.noreply.github.com>
2026-07-06 10:53:50 +0200

    Save page state in memory for self-hosted backends (#1193)

--- packages/ssr/src/rendering/head.ts
+++ packages/ssr/src/rendering/head.ts
@@ -179,7 +179,7 @@ export const getHeadItems = ({
     const manifestUrl = urlWithCacheBuster('/manifest.json', cacheBuster)
     headItems.set(
       'link:manifest',
-      `<link rel="manifest" href="${escapeAttrValue(manifestUrl)}">`,
+      `<link rel="manifest" href="${escapeAttrValue(manifestUrl)}" />`,
     )
   } else {
     // Only add a default theme-color + msapplication-TileColor if there is no manifest declared
@@ -247,19 +247,19 @@ export const getHeadItems = ({
   ) {
     headItems.set(
       'link:mask-icon',
-      '<link rel="mask-icon" href="https://raw.githubusercontent.com/nordcraftengine/resources/main/icons/safari-pinned-tab.svg" color="#171717">',
+      '<link rel="mask-icon" href="https://raw.githubusercontent.com/nordcraftengine/resources/main/icons/safari-pinned-tab.svg" color="#171717" />',
     )
     headItems.set(
       'link:apple-touch-icon',
-      '<link rel="apple-touch-icon" sizes="180x180" href="https://raw.githubusercontent.com/nordcraftengine/resources/main/icons/apple-touch-icon.png">',
+      '<link rel="apple-touch-icon" sizes="180x180" href="https://raw.githubusercontent.com/nordcraftengine/resources/main/icons/apple-touch-icon.png" />',
     )
     headItems.set(
       'link:icon:16',
-      '<link rel="icon" type="image/png" sizes="16x16" href="https://raw.githubusercontent.com/nordcraftengine/resources/main/icons/favicon-16x16.png">',
+      '<link rel="icon" type="image/png" sizes="16x16" href="https://raw.githubusercontent.com/nordcraftengine/resources/main/icons/favicon-16x16.png" />',
     )
     headItems.set(
       'link:icon:32',
-      '<link rel="icon" type="image/png" sizes="32x32" href="https://raw.githubusercontent.com/nordcraftengine/resources/main/icons/favicon-32x32.png">',
+      '<link rel="icon" type="image/png" sizes="32x32" href="https://raw.githubusercontent.com/nordcraftengine/resources/main/icons/favicon-32x32.png" />',
     )
   }
   // Handle custom meta tags last to allow overriding defaults
