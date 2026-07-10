f1c4f56faf9d8963bcedbe40d24e57cd16335f95
Erik Beuschau <bager.erik@gmail.com>
2026-07-10 13:34:42 +0200

    Revert no reference workflow rule description

--- packages/search/src/rules/issues/workflows/noReferenceComponentWorkflowRule.ts
+++ packages/search/src/rules/issues/workflows/noReferenceComponentWorkflowRule.ts
@@ -81,7 +81,7 @@ export const noReferenceComponentWorkflowRule: IssueRule<{
       path,
       info: {
         title: 'Unused component workflow',
-        description: `The **${workflowName}** workflow has a legacy name field. Consider migrating the workflow to use standard keys by renaming it or applying the rename fix.`,
+        description: `**${value.name}** is never used by any workflow. Consider removing it.`,
       },
       details: { contextSubscribers, name: workflowName },
     })
