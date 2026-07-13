7e38d9f166f1056154074ac4de5574e51758cff7
Jacob Kofoed <jacob.m.kofoed@gmail.com>
2026-07-13 22:00:17 +0200

    Issue rules use key as fallback name as name field is deprecated (#1226)

--- packages/search/src/rules/issues/workflows/noReferenceComponentWorkflowRule.ts
+++ packages/search/src/rules/issues/workflows/noReferenceComponentWorkflowRule.ts
@@ -76,14 +76,14 @@ export const noReferenceComponentWorkflowRule: IssueRule<{
         }
       }
     }
-    const workflowName = value.name ?? String(workflowKey)
+    const name = value.name ?? String(workflowKey)
     report({
       path,
       info: {
         title: 'Unused component workflow',
-        description: `**${value.name}** is never used by any workflow. Consider removing it.`,
+        description: `**${name}** is never used by any workflow. Consider removing it.`,
       },
-      details: { contextSubscribers, name: workflowName },
+      details: { contextSubscribers, name },
     })
   },
 }
